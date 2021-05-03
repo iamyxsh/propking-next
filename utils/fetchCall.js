e954685580c242aa30ec6fa4bf831a5b74054fb5
yash1798 <68080363+yash1798@users.noreply.github.com>
2021-05-03 12:26:53 +0530

    v1

--- utils/fetchCall.js
+++ utils/fetchCall.js
@@ -1,13 +1,16 @@
 const fetchCall = async (endpoint, method, jwt, body) => {
-	const data = await fetch(`http://localhost:5000/api/${endpoint}`, {
-		method: `${method}`,
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-			Authorization: jwt ? `Bearer ${jwt}` : "Bearer",
-		},
-		body: body ? JSON.stringify(body) : null,
-	}).then((response) => response.json())
+	const data = await fetch(
+		`https://propking-yxsh.herokuapp.com/api/${endpoint}`,
+		{
+			method: `${method}`,
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				Authorization: jwt ? `Bearer ${jwt}` : "Bearer",
+			},
+			body: body ? JSON.stringify(body) : null,
+		}
+	).then((response) => response.json())
 
 	return data
 }
